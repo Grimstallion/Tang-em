d4570afd68ec80f59db4ccd11d8b726d9285ac1a
Grimstallion <86743089+Grimstallion@users.noreply.github.com>
2021-07-01 02:23:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay anh qua đón đi chơi nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun <33',
     messLink: 'https://www.facebook.com/profile.php?id=100006375904753' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
